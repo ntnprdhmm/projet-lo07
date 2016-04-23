95a22e786254a3b902abc57ff09486b80277b436
prudywsh <antoineprudhomme5@gmail.com>
2016-04-23 17:00:24 +0200

    Bootstrap et jQuery installes.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,46 +2,15 @@
 <html ng-app="myApp">
     <head>
         <title>Laravel</title>
-
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
+        <link href="../node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" type="text/css">
     </head>
     <body>
-        <div class="container">
-            <div class="content" ng-controller="helloWorldController as helloCtrl">
-                <div class="title">@{{ helloCtrl.hello }}</div>
-            </div>
+        <div class="container" ng-controller="helloWorldController as helloCtrl">
+            <h1>@{{ helloCtrl.hello }}</h1>
         </div>
     </body>
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular.min.js"></script>
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="../node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="../node_modules/angular/angular.min.js"></script>
     <script type="text/javascript" src="js/app.js"></script>
 </html>
