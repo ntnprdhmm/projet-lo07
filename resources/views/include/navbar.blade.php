024b4aa7ce0533c0c6463807a9229f8adac8d05d
prudywsh <antoineprudhomme5@gmail.com>
2016-04-26 15:37:49 +0200

    Encore du style, mais ce coup ci c'est bon (pour l'index ... ;))

--- resources/views/include/navbar.blade.php
+++ resources/views/include/navbar.blade.php
@@ -8,7 +8,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Institut Charles Delaunay</a>
+            <a class="navbar-brand" href="#">ICD</a>
         </div>
 
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
