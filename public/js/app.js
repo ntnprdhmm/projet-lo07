47a34d49827e89b623eb47559f52d7cb57f9a9a5
prudywsh <antoineprudhomme5@gmail.com>
2016-05-04 17:10:47 +0200

    Controller angular pour l'affichage d'une publication.

--- public/js/app.js
+++ public/js/app.js
@@ -9,8 +9,9 @@ app.config(['$routeProvider',
         when('/publications/:page', {
             templateUrl: 'js/templates/pages/publications/index.html'
         }).
-        when('/publications/:pages/publication/:id', {
-            templateUrl: 'js/templates/pages/publications/publication.html'
+        when('/publications/:page/publication/:id', {
+            templateUrl: 'js/templates/pages/publications/publication.html',
+            controller: 'publicationController'
         }).
         when('/inscription', {
             templateUrl: 'js/templates/pages/inscription/index.html'
@@ -20,9 +21,20 @@ app.config(['$routeProvider',
         });
     }]);
 
+/**
+ * Concerne l'ensemble des publications
+ */
 app.controller('publicationsController', function($scope) {
     $scope.displayForm = false;
     $scope.toggleForm = function() {
         $scope.displayForm = !$scope.displayForm;
     };
+});
+
+/**
+ * Concerne une seule publication
+ */
+app.controller('publicationController',function($scope, $routeParams) {
+    $scope.page = $routeParams.page;
+    $scope.id = $routeParams.id;
 });
