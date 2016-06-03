20ab20b964666332d3c6164a586ad1e4bcb761d7
prudywsh <antoineprudhomme5@gmail.com>
2016-06-03 10:07:16 +0200

    Petit correctif les conferences (categorie de publication)

--- public/js/controllers/NewPublicationCtrl.js
+++ public/js/controllers/NewPublicationCtrl.js
@@ -8,8 +8,7 @@ app.controller('NewPublicationCtrl', function($scope) {
         "OS : Ouvrage Scientifique (Chapitre de Livre, ...)",
         "TD : Thèse de Doctorat",
         "BV : Brevet",
-        "AP : Autre Production",
-        "Conférence"
+        "AP : Autre Production"
     ];
 
 });
