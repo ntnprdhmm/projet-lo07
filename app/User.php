f7a0668e18ab83da5072d086729ff803a118e314
PunKeel <punkeel@me.com>
2016-06-12 04:54:41 +0200

    :o

--- app/User.php
+++ app/User.php
@@ -14,7 +14,8 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name',
+        'nom',
+        'prenom',
         'email',
         'password',
     ];
