78c15498ebb4659ed42092a6fdf67f2b54d72934
PunKeel <punkeel@me.com>
2016-06-12 03:57:05 +0200

    fix routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,8 +1,10 @@
 <?php
 
-Route::get('/', function () {
+Route::get(
+    '/', function () {
     return view('index');
-});
+}
+);
 
 Route::post('login', 'UserController@login');
 Route::post('api/register', 'UserController@register');
@@ -10,5 +12,5 @@ Route::post('api/register', 'UserController@register');
 Route::get('api/admin/accounts', 'AdminController@accounts');
 
 Route::get('api/categories', 'CategorieController@getCategories');
-Route::post('getLaboratoires', 'LaboratoireController@getLaboratoires');
-Route::post('getPublications', 'PublicationController@getPublications');
+Route::post('api/laboratoires', 'LaboratoireController@getLaboratoires');
+Route::post('api/publications', 'PublicationController@getPublications');
