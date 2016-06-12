da9764d16a20ed489b7cacdfcefd863bd48dbb56
PunKeel <punkeel@me.com>
2016-06-12 03:47:35 +0200

    Use GET

--- public/js/controllers/AdminCtrl.js
+++ public/js/controllers/AdminCtrl.js
@@ -1,7 +1,7 @@
 app.controller('AdminCtrl', function ($scope, $http) {
 
-    $http({method: 'post', url: 'api/admin/accounts'}).success(function (data) {
+    $http({method: 'get', url: '/api/admin/accounts'}).success(function (data) {
         $scope.users = data.accounts;
     });
-    
+
 });
