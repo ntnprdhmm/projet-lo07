05849b46a6ecee7b6e731e90c4f70c98a8054599
PunKeel <punkeel@me.com>
2016-06-12 04:53:32 +0200

    xx

--- app/Auteur.php
+++ app/Auteur.php
@@ -8,11 +8,13 @@ class Auteur extends Model
 {
     protected $table = 'auteurs';
     public $timestamps = false;
+    protected $fillable = array('user_id', 'publication_id');
 
     public function publication()
     {
         return $this->belongsTo('App\Publication');
     }
+
     public function user()
     {
         return $this->belongsTo('App\User');
