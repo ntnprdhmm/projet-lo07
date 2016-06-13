800ecaf82e1e7245ec476f2c8bbefef069c1c5e6
PunKeel <punkeel@me.com>
2016-06-13 02:56:20 +0200

    xxx

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,10 +10,11 @@ Route::post('login', 'UserController@login');
 Route::post('api/register', 'UserController@register');
 
 Route::get('api/admin/accounts', 'AdminController@accounts');
+Route::get('api/anomalies', 'AdminController@anomalies');
+
 Route::get('api/me', 'UserController@me');
 
 Route::get('api/categories', 'CategorieController@getCategories');
 Route::get('api/laboratoires', 'LaboratoireController@getLaboratoires');
 Route::get('api/publications', 'PublicationController@getPublications');
 Route::put('api/publications', 'PublicationController@addPublications');
-Route::get('api/anomalies', 'PublicationController@findAnomalies');
